1d882d34bbfaa198c9ddf9463a3328c76243c2cd
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-20 08:51:33 +0800

    [PERF] add display cpuset
    Change-Id: I859d327c7a4719d9cdda4436ceb3a6b6244d7f15
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- libs/renderengine/threaded/RenderEngineThreaded.cpp
+++ libs/renderengine/threaded/RenderEngineThreaded.cpp
@@ -79,7 +79,7 @@ status_t RenderEngineThreaded::setSchedFifo(bool enabled) {
 void RenderEngineThreaded::threadMain(CreateInstanceFactory factory) NO_THREAD_SAFETY_ANALYSIS {
     SFTRACE_CALL();
 
-    if (!SetTaskProfiles(0, {"SFRenderEnginePolicy"})) {
+    if (!SetTaskProfiles(0, {"DisplayPolicy"})) {
         ALOGW("Failed to set render-engine task profile!");
     }
 
